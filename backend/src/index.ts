6032327df6e01b103037353cef04acef1e2f718d
Simone Sestito <simone@simonesestito.com>
2022-03-23 08:38:15 +0100

    Disable express HTTP cache

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import nocache from 'nocache';
 dotenv.config({ override: true });
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(nocache());
+app.set('etag', false);
 app.use(express.static(
-    path.join(__dirname, '..', '..', 'webapp', 'build', 'web')
+    path.join(__dirname, '..', '..', 'webapp', 'build', 'web'),
+    { etag: false },
 ));
 
 app.listen(PORT, () => console.log('Server running!'));

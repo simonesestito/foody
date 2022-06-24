fa8fdb8adb218c4d13267baf233046f003fa5b52
Simone Sestito <simone@simonesestito.com>
2022-06-24 11:41:43 +0200

    Deploy script: remove env vars fix
    Unnecessary after https://github.com/simonesestito/docker-projects-manager/commit/677e52eb4f6129df1e679dfe2ea4ac04a6bd0dda

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
+#
+# Private deploy script, in order to:
+# - push local commits to GitHub
+# - build and push a new Docker image to Docker Hub
+# - Access the server via SSH and run the update script there
+#
+
+
 PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$PROJECT_DIR"
 git push
 docker build -t simonesestito/foody:latest "$PROJECT_DIR"
 docker push simonesestito/foody:latest
 ssh foody ./projects-manager update foody
-# FIXME: Fix env vars processing
-ssh foody mv foody/.env.bak foody/.env
-ssh foody docker compose --project-directory foody down
-ssh foody docker compose --project-directory foody up -d
